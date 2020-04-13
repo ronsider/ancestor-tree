90725d84a87fe28e4875499c4d15a824fe2eadc6
ronsider <44822122+ronsider@users.noreply.github.com>
2020-04-13 19:37:35 +0300

    Update FamilyTree.hpp

--- FamilyTree.hpp
+++ FamilyTree.hpp
@@ -15,7 +15,7 @@ namespace family
   Tree addFather(string son,string father);
   Tree addMother(string son,string mother);
   void display() const;//const indicates read only function
-  Tree relation(string name) const;//return the node that correspond to this name
-  void find(string s) const;//relation based on name
+  Tree relation(string name) const;//return the relation that correspond to this name and root
+  Tree find(string s) const;//put name and get the relation
  }
 }
